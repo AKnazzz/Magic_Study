960dd6b10c876443948ceb8357f35546d4b74b4b
Andrey Knyazev <AKAKnaz@yandex.ru>
2023-11-20 01:19:28 +0300

    les2

--- src/main/java/org/hogwarts/Lesson2/PrimitiveDataTypes.java
+++ src/main/java/org/hogwarts/Lesson2/PrimitiveDataTypes.java
@@ -4,6 +4,49 @@ public class PrimitiveDataTypes {
     public static void main(String[] args) {
 
         byte b1 = 10;
+        byte b2 = 20;
+        byte b3 = 100;
+
+        short s1 = 5;
+        short s2 = -10;
+        short s3 = 0;
+
+        int i1 = 500;
+
+        long l1 = 1000;
+        long l2 = 10000000000L;
+        long l3 = 51L;
+
+        float f1 = 3.14f;
+        float f2 = 2.5f;
+
+        double d1 = 5.5;
+        double d2 = 87.65;
+
+        char c1 = 'a';
+        char c2 = 'A';
+        char c3 = '7';
+        char c4 = ' ';
+        char c5 = 300;  // ==> номер символа из UNICODE 10
+        char c6 = '\u0500'; // ==> номер символа из UNICODE 16
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+
+        int a1 = 60;  // десятичная
+
+        int a2 = 0b00111100;  // двоичная 0b+...
+        int a21 = 0B00111100;  // двоичная
+
+        int a3 = 074;  // восьмеричная  0...
+
+        int a4 = 0x3c;  // шестнадцатеричная
+        int a41 = 0X3C;  // шестнадцатеричная
+
+
+
+
 
     }
 }
