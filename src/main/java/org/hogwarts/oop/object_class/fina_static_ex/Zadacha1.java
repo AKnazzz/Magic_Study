03645b94272aee215dc801c4ee1ca273e12df750
Andrey Knyazev <AKAKnaz@yandex.ru>
2023-12-04 00:47:37 +0300

    add tasks by constatnts

---
+++ src/main/java/org/hogwarts/oop/object_class/fina_static_ex/Zadacha1.java
@@ -0,0 +1,25 @@
+package org.hogwarts.oop.object_class.fina_static_ex;
+
+public class Zadacha1 {
+
+    static double umnojenie (double a, double b, double c){
+        System.out.println("Умножаем 3 числа: " + a + ", "+  b + ", "+ c);
+        System.out.println("Результат: " + a*b*c);
+        return a*b*c;
+    }
+
+    static void delenie (int a, int b){
+        System.out.println("Целое частное = " + a/b + " ;");
+        System.out.println("Остаток = " + a%b + " ;");
+    }
+
+}
+
+class Zadacha1Test {
+    public static void main(String[] args) {
+        Zadacha1.umnojenie(2,3,5);
+        Zadacha1.delenie(5,2);
+        Zadacha1.umnojenie(2.5,2,3);
+        Zadacha1.delenie(7,3);
+    }
+}
