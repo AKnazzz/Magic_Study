607806c0ccd2e366596c06cf9d49f1dc71210c50
Andrey Knyazev <AKAKnaz@yandex.ru>
2023-12-11 01:01:59 +0300

    add README fore some tasks

---
+++ src/main/java/org/hogwarts/if_else/task2/TaskFromHogwarstIfElse2.java
@@ -0,0 +1,8 @@
+package org.hogwarts.if_else.task2;
+
+public class TaskFromHogwarstIfElse2 {
+    public static void main(String[] args) {
+        SoringHat soringHat = new SoringHat();
+        System.out.println(soringHat.sort());
+    }
+}
