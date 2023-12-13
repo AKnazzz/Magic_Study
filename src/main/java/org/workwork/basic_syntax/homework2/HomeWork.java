2dac8e87b726ff8fb087230741e1b2e9ce87d6e5
Andrey Knyazev <AKAKnaz@yandex.ru>
2023-12-14 01:04:58 +0300

    add newWorkTasks = 3 of 5

---
+++ src/main/java/org/workwork/basic_syntax/homework2/HomeWork.java
@@ -0,0 +1,29 @@
+package org.workwork.basic_syntax.homework2;
+
+import java.util.Scanner;
+
+/**
+ * 1. Создать программу, которая будет считывать информацию о вашем друге из консоли и распечатывать результат обратно в консоль.
+ * <p>
+ * > Расскажите о вашем друге
+ * > Как зовут вашего друга
+ * > Боря
+ * > Сколько лет вашему другу
+ * > 20
+ * > Моему другу <Боря> сейчас <20> лет
+ */
+
+public class HomeWork {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Расскажите о вашем друге");
+        System.out.println("Как зовут вашего друга ?");
+        String friendName = scanner.nextLine();
+
+        System.out.println("Сколько лет вашему другу?");
+        int friendAge = scanner.nextInt();
+
+        System.out.println("Моему другу с именем" + friendName + " сейчас " + friendAge + " лет.");
+    }
+}
