9b2363d267c4f975ebba3040a51598c5f8b32ffe
Andrey Knyazev <AKAKnaz@yandex.ru>
2023-12-19 16:49:54 +0300

    add: WarokWork Tasks

---
+++ src/main/java/org/workwork/stage_one_base/homework_5/HomeWork2.java
@@ -0,0 +1,36 @@
+package org.workwork.stage_one_base.homework_5;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        int n = 28;
+
+//        for (int i = 0; i <= n; i++) {
+//            if (i > 0 && i % 3 == 0) {
+//                System.out.print("fizz");
+//            }
+//            if (i > 0 && i % 5 == 0) {
+//                System.out.print("buzz");
+//            }
+//            if (i == 0 || (i % 3 != 0 && i % 5 !=0)) {
+//                System.out.print(i);
+//            }
+//            System.out.print(" ");
+//        }
+
+        for (int i = 0; i <= n; i++) {
+            if (i == 0) {
+                System.out.print(i);
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.print("fizz");
+            } else if (i % 5 == 0) {
+                System.out.print("buzz");
+            } else {
+                System.out.print(i);
+            }
+            System.out.print(" ");
+
+        }
+    }
+}
