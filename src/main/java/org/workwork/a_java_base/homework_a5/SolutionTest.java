e8d026bde9a14a514611132466940e4ed586e27f
Andrey Knyazev <AKAKnaz@yandex.ru>
2023-12-28 09:34:32 +0300

    add solution a_5

---
+++ src/main/java/org/workwork/a_java_base/homework_a5/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.workwork.a_java_base.homework_a5;
+
+import java.util.Arrays;
+
+public class SolutionTest {
+    public static void main(String[] args) {
+
+        char[] s = {'H','e','l','l','o'};
+
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(s));
+        solution.reverseString(s);
+        System.out.println(Arrays.toString(s));
+
+        System.out.println("=======================");
+
+        Solution2 solution2 = new Solution2();
+        System.out.println(Arrays.toString(s));
+        solution2.reverseString(s);
+        System.out.println(Arrays.toString(s));
+
+        System.out.println("=======================");
+        
+        Solution3 solution3 = new Solution3();
+        System.out.println(Arrays.toString(s));
+        solution3.reverseString(s);
+        System.out.println(Arrays.toString(s));
+
+
+    }
+}
