93e50e1a1a875d6eb7d7349a393e01426f32e0e4
Andrey Knyazev <AKAKnaz@yandex.ru>
2024-01-14 00:09:12 +0300

    ADD 14/01/2024

---
+++ src/main/java/org/workwork/b_java_objects/homework_b3/solution/circle/Main.java
@@ -0,0 +1,8 @@
+package org.workwork.b_java_objects.homework_b3.solution.circle;
+
+public class Main {
+    public static void main(String[] args) {
+        TestCircle test = new TestCircle();
+        test.test();
+    }
+}
