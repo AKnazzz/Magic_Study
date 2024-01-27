942225f68a9d532235e583ebf61f0050b39acac0
Andrey Knyazev <AKAKnaz@yandex.ru>
2024-01-28 00:01:49 +0300

    28.01.2024 ADD Calculator

--- src/main/java/org/workwork/b_java_objects/homework_b7/calculator/Calculator.java
+++ src/main/java/org/workwork/b_java_objects/homework_b7/calculator/Calculator.java
@@ -8,53 +8,101 @@ public class Calculator {
     private static final char MULTIPLICATION = '*';
     private static final char DIVISION = '/';
 
+    private static final Double[] results = new Double[10];
+
     public static void main(String[] args) {
 
-        double[] numbers = new double[10];
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
 
-        System.out.println("Введите 2 числа ");
-        double first = readNumber();
-        double second = readNumber();
+        for (; ; ) {
 
-        System.out.print("Введите оператор: (+, -, *, /): ");
-        char operator = readOperator();
+            System.out.printf("Память заполнена на %d / 9%n", i);
 
-        double result = calculation(first, second, operator);
-        System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result);
+            if (i == 9) {
+                print();
+            }
 
-    }
+            System.out.println("Введите 'выход' для завершения ");
 
-    public static double readNumber() {
-        try (Scanner reader = new Scanner(System.in)) {
-            return reader.nextDouble();
-        }
-    }
+            String s = scanner.nextLine();
+            if (s.equalsIgnoreCase("выход")) {
+                print();
+                break;
+            } else {
+                System.out.println("Тогда продолжаем");
+            }
+            
+            double first = 0;
+            double second = 0;
+            boolean inputValid = false;
 
-    public static char readOperator() {
-        try (Scanner reader = new Scanner(System.in)) {
-            return reader.next().charAt(0);
-        }
-    }
-
-    public static double calculation(double first, double second, char operator) {
-        switch (operator) {
-            case ADDITION:
-                return first + second;
-
-            case SUBTRACTION:
-                return first - second;
-
-            case MULTIPLICATION:
-                return first * second;
-
-            case DIVISION:
-                if (second == 0) {
-                    throw new IllegalArgumentException("Деление на ноль!");
+            while (!inputValid) {
+                System.out.println("Введите 2 числа ");
+                if (scanner.hasNextDouble()) {
+                    first = scanner.nextDouble();
+                    if (scanner.hasNextDouble()) {
+                        second = scanner.nextDouble();
+                        inputValid = true;
+                    } else {
+                        System.out.println("Ошибка: второе значение не является числом");
+                        scanner.next();
+                    }
+                } else {
+                    System.out.println("Ошибка: первое значение не является числом");
+                    scanner.next();
                 }
-                return first / second;
+            }
 
-            default:
-                throw new IllegalArgumentException("Введите корректный оператор");
+            System.out.print("Введите оператор: (+, -, *, /): ");
+            char operator = scanner.next().charAt(0);
+
+
+            double result = 0.0;
+
+
+            switch (operator) {
+                case ADDITION:
+                    result = first + second;
+                    System.out.printf("%.1f %c %.1f = %.3f%n", first, operator, second, result);
+                    i++;
+                    results[i] = result;
+                    break;
+
+                case SUBTRACTION:
+                    result = first - second;
+                    System.out.printf("%.1f %c %.1f = %.3f%n", first, operator, second, result);
+                    i++;
+                    results[i] = result;
+                    break;
+
+                case MULTIPLICATION:
+                    result = first * second;
+                    System.out.printf("%.1f %c %.1f = %.3f%n", first, operator, second, result);
+                    i++;
+                    results[i] = result;
+                    break;
+
+                case DIVISION:
+                    if (second == 0) {
+                        throw new IllegalArgumentException("Деление на ноль!");
+                    }
+                    result = first / second;
+                    System.out.printf("%.1f %c %.1f = %.3f%n", first, operator, second, result);
+                    i++;
+                    results[i] = result;
+                    break;
+
+                default:
+                    throw new IllegalArgumentException("Введите корректный оператор");
+            }
+        }
+
+    }
+
+    private static void print() {
+        for (Double result : results) {
+            System.out.println(result);
         }
     }
 
