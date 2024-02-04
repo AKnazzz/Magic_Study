aab90469cc98dc5cd13651fa772abc35f9946e0f
Andrey Knyazev <AKAKnaz@yandex.ru>
2024-02-05 00:37:08 +0300

    05.02.2024 ADD CODEWARS 2

---
+++ src/main/java/org/orders/war/task_8/YesOrNo.java
@@ -0,0 +1,14 @@
+package org.orders.war.task_8;
+
+/**
+ * Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+ */
+public class YesOrNo {
+    public static String boolToWord(boolean b) {
+        if (b) {
+            return "Yes";
+        } else
+            return "No";
+    }
+}
+
