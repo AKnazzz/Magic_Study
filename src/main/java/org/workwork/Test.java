e277d05bfede0897180d64a9248ce6f4b91e9006
Andrey Knyazev <AKAKnaz@yandex.ru>
2024-02-20 22:44:33 +0300

    ADD 20/02/2024

--- src/main/java/org/workwork/Test.java
+++ src/main/java/org/workwork/Test.java
@@ -1,10 +1,18 @@
 package org.workwork;
 
+import java.util.ArrayList;
+
 public class Test {
     public static void main(String[] args) {
         char[] textArray = {'0','1','2','3','4','5','6','7','8','9'};
         String newString = String.copyValueOf(textArray,1,3);
         System.out.println(newString);
+
+        for (int i = 0; i <= textArray.length; i++) {
+            System.out.println(textArray[i]);
+        }
+
+       // ArrayList<int> arrayList = new ArrayList<int>();
 
     }
 }
