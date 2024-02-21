b92a467bf0ed791a5f980714b4dedffd6477b144
Andrey Knyazev <AKAKnaz@yandex.ru>
2024-02-21 16:54:39 +0300

    ADD TASKS YANDEX 21.02.24

---
+++ src/main/java/org/ya/module_0/task_3_3_1/Praktikum.java
@@ -0,0 +1,53 @@
+package org.ya.module_0.task_3_3_1;
+
+import java.util.Scanner;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        System.out.println("Робот-помощник v2.0.");
+        // Вызовите ниже методы в правильном порядке
+        sayHello();
+        sayHelloByTime();
+        welcomeUserByName();
+        printSuccess();
+    }
+
+    public static void welcomeUserByName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Как вас зовут?");
+        String name = scanner.nextLine();
+        // Допишите чтение города
+        printCity();
+        String city = scanner.nextLine();
+        System.out.println("Рад познакомиться, " + name + " из " + city + "!");
+    }
+
+    public static void printSuccess() {
+        System.out.println("У вас уже неплохо получается программировать!");
+    }
+
+    // Допишите метод sayHello(), который печатает: Привет!
+    public static void sayHello() {
+        System.out.println("Привет!");
+    }
+    // Допишите метод printCity(), который печатает: Из какого вы города?
+    public static void printCity() {
+        System.out.println("Из какого вы города?");
+    }
+    public static void sayHelloByTime() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Который час?");
+        int currentHour = scanner.nextInt();
+        if (currentHour < 6) {
+            System.out.println("Доброй ночи!");
+        } else if (currentHour > 22) {
+            System.out.println("Доброй ночи!");
+        } else if (currentHour < 12) {
+            System.out.println("Доброе утро!");
+        } else if (currentHour < 18) {
+            System.out.println("Добрый день!");
+        } else {
+            System.out.println("Добрый вечер!");
+        }
+    }
+}
