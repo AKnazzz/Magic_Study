66bb94b9ab688951b203361ff6523b259de15fcd
Andrey Knyazev <AKAKnaz@yandex.ru>
2024-03-20 16:35:11 +0300

    ADD 20.03.2024

--- src/main/java/org/workwork/Main.java
+++ src/main/java/org/workwork/Main.java
@@ -1,4 +1,40 @@
 package org.workwork;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1,3,5,3,3,6,9,6,0,1));
+        int k = 2;
+
+        getRollingAverage(arr, k);
+    }
+
+    private static void getRollingAverage(ArrayList<Integer> arr, int k) {
+        LinkedList<Float> sublist = new LinkedList<>();
+
+        for (int i = 1; i < k; i++) {
+            float x = arr.get(i);
+            float y = arr.get(i - 1);
+            float z = (x + y)/2;
+
+            sublist.add(z);
+        }
+
+        System.out.println(sublist);
+
+        for (int i = k; i <arr.size(); i++) {
+            float x = arr.get(i);
+            float y = arr.get(i - 1);
+            float z = (x + y)/2;
+
+            sublist.add(z);
+            sublist.remove();
+            System.out.println(sublist);
+        }
+
+    }
 }
