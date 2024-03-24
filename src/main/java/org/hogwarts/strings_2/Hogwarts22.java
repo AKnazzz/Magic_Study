f5d3d8a5e01857f0f20b7502e4a41570e85dfff0
Andrey Knyazev <AKAKnaz@yandex.ru>
2024-03-24 20:54:23 +0300

    ADD 24.03.2024 Hogwarts 33

--- src/main/java/org/hogwarts/strings_2/Hogwarts22.java
+++ src/main/java/org/hogwarts/strings_2/Hogwarts22.java
@@ -2,7 +2,7 @@ package org.hogwarts.strings_2;
 
 import java.util.Random;
 
-public class Task_String_2 {
+public class Hogwarts22 {
     public static void main(String[] args) {
         castSpell();
 
@@ -10,24 +10,21 @@ public class Task_String_2 {
 
     public static void castSpell() {
         //1. Создайте строку в верхнем регистре: ARDENTIS VERUM LUMINOS ET FULGUR SYLVESTRA ELIXIA.
-        //2. Приведите строку к нижнему регистру
         String spellString = "ARDENTIS VERUM LUMINOS ET FULGUR SYLVESTRA ELIXIA";
+
+        //2. Приведите строку к нижнему регистру
         String lowercaseSpellString = spellString.toLowerCase();
 
         //3. Создайте случайное число int start с помощью класса Random. Число должно быть в диапазоне от 0 до 50.
-
-        int start = new Random().nextInt(51);
+        int start = new Random().nextInt(50);
 
         //4. Создайте подстроку от строки, содержащей слова заклинаний, которая будет начинаться с индекса, равного start.
-
         String subString = lowercaseSpellString.substring(start);
 
         //5. Создайте объект класса StringBuilder spell, запишите полученную строку выше в этот объект.
-
         StringBuilder spell = new StringBuilder(subString);
 
         //6. Разверните строку задом наперед.
-
         spell.reverse();
 
         //7. В полученной строке поменяйте местами символы под 0 и 1 индексами.
@@ -43,15 +40,12 @@ public class Task_String_2 {
         spell.setCharAt(0, one);
         spell.setCharAt(1, zero);
 
-//        spell.replace(0, 1, String.valueOf(one));
-//        spell.replace(1, 2, String.valueOf(zero));
 
         //8. Полученную строку выведите на экран. Запускаем метод main.
 
         System.out.println(spell.toString());
 
         //Прочитайте заклинание! Попробуйте запустить несколько раз и посмотрите, как меняется ваше заклинание. Подумайте, почему?
-
 
     }
 }
