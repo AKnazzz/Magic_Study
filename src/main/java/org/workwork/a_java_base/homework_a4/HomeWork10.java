655890de03a7a39ad488e23b0ae9045845879563
Andrey Knyazev <AKAKnaz@yandex.ru>
2024-04-02 00:00:40 +0300

    ADD 02.04.2024 WorkWork

---
+++ src/main/java/org/workwork/a_java_base/homework_a4/HomeWork10.java
@@ -0,0 +1,18 @@
+package org.workwork.a_java_base.homework_a4;
+
+public class HomeWork10 {
+    public static void main(String[] args) {
+        int n = 15;
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("fizzbuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
