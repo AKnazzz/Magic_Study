264929bfab6bdec4155c6883e495e6d8add68c77
Andrey Knyazev <AKAKnaz@yandex.ru>
2024-04-23 01:16:15 +0300

    ADD 23.04.2024 WorkWork 22

--- src/main/java/org/workwork/Main.java
+++ src/main/java/org/workwork/Main.java
@@ -1,48 +1,47 @@
 package org.workwork;
 
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
-
     public static void main(String[] args) {
-        try {
-            List<Double> maxLength = new BufferedReader(new FileReader("C:\\Users\\Андрей\\dev\\magic\\Magic\\src\\main\\java\\org\\workwork\\data.txt"))
-                    .lines()
-                    .map(s->{
-                        String x1 = s.substring(17,20);
-                        String numberAsStringX = s.toString();
-                        String x2 = numberAsStringX.substring(30,32);
-                        Double X12 = Math.pow(Double.valueOf(x2) -Double.valueOf(x1),2);
-
-                        String y1 = s.substring(23,25);
-                        String numberAsStringY = s.toString();
-                        String y2 = numberAsStringY.substring(35,37);
-                        Double y12 = Math.pow(Double.valueOf(y2) -Double.valueOf(y1),2);
-                        Double XY12 = Math.sqrt(X12 + y12);
-                        String XY = String.valueOf(XY12);
-
-                        return Double.parseDouble(XY);
-                    }).collect(Collectors.toList());
-
-            maxLength.stream()
-                    .max(Double::compare);
-            System.out.println(" MAX - " + maxLength.stream()
-                    .max(Double::compare));
-
-            maxLength.stream()
-                    .forEach(s-> System.out.println(s));
-
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
+        ArrayList<Double> listInteger = new ArrayList<>(Arrays.asList(1.0, 7.0, 9.0, 4.0, 3.0));
+        int number = 4;
+        System.out.println(getRollingAverage(listInteger, number));
     }
+//    public static void printSubbary(ArrayList<Integer> list, int number){
+//        LinkedList<Integer> linkedList = new LinkedList<>();
+//        ArrayList<Double> resultList = new ArrayList<>();
+//        double result;
+//        for(int i = 0; i < number; i++){
+//            linkedList.add(list.get(i));
+//        }
+//        result = (linkedList.get(0) + linkedList.get(1)) / 2.0;
+//        resultList.add(result);
+//
+//
+//        for(int i = number; i < list.size(); i++ ){
+//            linkedList.add(list.get(i));
+//            linkedList.remove();
+//            result = (linkedList.get(0) + linkedList.get(1)) / 2.0;
+//            resultList.add(result);
+//
+//        }
+//        System.out.println(resultList);
+//    }
 
+    public static List<Double> getRollingAverage(ArrayList<Double> arr, int k) {
+        List<Double> result = new ArrayList<>();
+        for (int i = 0; i <= arr.size() - k; i++) {
+            double sum = 0;
+            for (int j = i; j < i + k; j++) {
+                sum += arr.get(j);
+            }
+            result.add(sum / k);
+        }
+        return result;
+    }
 }
