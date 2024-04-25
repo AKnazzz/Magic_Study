a97a86b980c45c8595df77938eaeaaaccd9a1d1f
Andrey Knyazev <AKAKnaz@yandex.ru>
2024-04-26 00:02:02 +0300

    ADD 26.04.2024

--- src/main/java/org/workwork/Main.java
+++ src/main/java/org/workwork/Main.java
@@ -8,9 +8,33 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Double> listInteger = new ArrayList<>(Arrays.asList(1.0, 7.0, 9.0, 4.0, 3.0));
-        int number = 4;
-        System.out.println(getRollingAverage(listInteger, number));
+//        ArrayList<Double> listInteger = new ArrayList<>(Arrays.asList(1.0, 7.0, 9.0, 4.0, 3.0));
+//        int number = 4;
+//        System.out.println(getRollingAverage(listInteger, number));
+
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1 ,2 ,3, 5));
+        int k = 1;
+        searchForTheAverage(arr, k);
+    }
+
+    private static void searchForTheAverage(ArrayList<Integer> arr, int k) {
+
+        LinkedList<Double> average = new LinkedList<>();
+
+        double av = 0;
+        for (int i = 0; i < k; i++) {
+            av = av + arr.get(i);
+        }
+        average.add(av / k);
+
+        int j = k-1;
+        for (int i = k; i < arr.size(); i++, j++) {
+            av = 0;
+            av = arr.get(j) + arr.get(i);
+            average.add(av/k);
+        }
+        System.out.println("Исходный массив:" + arr);
+        System.out.println("Массив из средних значений исходного:" + average);
     }
 //    public static void printSubbary(ArrayList<Integer> list, int number){
 //        LinkedList<Integer> linkedList = new LinkedList<>();
