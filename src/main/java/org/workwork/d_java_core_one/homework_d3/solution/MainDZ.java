76dd6e6eb61b4f448bdaee5d0ba950fae35b9fde
Andrey Knyazev <AKAKnaz@yandex.ru>
2024-05-29 00:34:17 +0300

    ADD 29.05.2024 HAPPY BIRTHDAY !!!

---
+++ src/main/java/org/workwork/d_java_core_one/homework_d3/solution/MainDZ.java
@@ -0,0 +1,34 @@
+package org.workwork.d_java_core_one.homework_d3.solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainDZ {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(List.of(1, 7, 9, 4, 3));
+        int k = 4;
+
+        List<Double> rollingAverages = getRollingAverage(arr, k);
+        System.out.println(rollingAverages);
+    }
+
+    public static List<Double> getRollingAverage(List<Integer> arr, int k) {
+        if (arr.size() < k || k == 0) {
+            throw new IllegalArgumentException(
+                    "Array size must be greater than or equal to k, and k must be non-zero.");
+        }
+
+        List<Double> result = new ArrayList<>();
+
+        for (int i = 0; i <= arr.size() - k; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k; j++) {
+                sum += arr.get(j);
+            }
+            double average = (double) sum / k;
+            result.add(average);
+        }
+        return result;
+    }
+
+}
