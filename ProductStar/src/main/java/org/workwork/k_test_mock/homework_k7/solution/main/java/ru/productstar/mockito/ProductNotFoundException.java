c8717726fe13f5959b8b361c40eea7400a85f0f2
Andrey Knyazev <AKAKnaz@yandex.ru>
2024-06-16 16:51:32 +0300

    16.06.2024 Sunday 55

--- ProductStar/src/main/java/org/workwork/k_test_mock/homework_k7/solution/main/java/ru/productstar/mockito/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.workwork.k_test_mock.homework_k7.solution.main.java.ru.productstar.mockito;
-
-public class ProductNotFoundException extends Exception {
-    public ProductNotFoundException(String product) {
-        super(product + " not found");
-    }
-}
